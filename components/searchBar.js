55f8b3702dc04c4b23d190e0fac2351cf8b3eeef
Aaron Anglin <aaron.anglin101@gmail.com>
2023-03-09 11:12:29 -0600

    fixed input styling and issues

---
+++ components/searchBar.js
@@ -0,0 +1,56 @@
+import React, {useState} from 'react'
+import axios from "axios";
+import { HiSearch } from "react-icons/hi";
+
+function searchBar() {
+    const [query, setQuery] = useState('')
+    const [recipe, setRecipe] = useState("");
+    const [loading, setLoading] = useState(false);
+    
+    const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${process.env.API_ID}&app_key=${process.env.API_KEY}`
+    
+    const getRecipe = (e) => {
+      e.preventDefault();
+      setLoading(true);
+      axios
+        .get(url, {
+          headers: {
+            'Access-Control-Allow-Origin': '*'
+          }
+        })
+        .then((response) => {
+          setRecipe(response.data);
+          console.log(response);
+          setQuery("");
+          setLoading(false);
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    };
+  return (
+    <div className="p-4 justify-center">
+    <div className="relative flex items-center justify-center max-w-[500px] w-full m-auto pt-4 text-white z-10 ">
+      <form
+        onSubmit={getRecipe}
+        className="flex justify-between items-center w-full m-auto p-3 bg-transparent border-2 border-gray-400 text-gray-400  rounded-2xl"
+      >
+        <div>
+          <input
+            onChange={(e) => setQuery(e.target.value)}
+            className="bg-transparent border-none text-gray-400 focus:outline-none text-2xl placeholder:text-white/15"
+            type="text"
+            placeholder="Search for a recipe"
+            value={query}
+          />
+        </div>
+        <button onClick={getRecipe}>
+          <HiSearch size={25} />
+        </button>
+      </form>
+    </div>
+    </div>
+  )
+}
+
+export default searchBar
--- src/pages/search.js
+++ src/pages/search.js
@@ -1,40 +1,13 @@
 import Menubar from '../../components/menubar';
-import React, {useState} from 'react'
+import React from 'react'
 import Navbar from '../../components/navbar';
 import Head from 'next/head';
-import axios from "axios";
-import { HiSearch } from "react-icons/hi";
-import Results from '../../components/results';
+import SearchBar from '../../components/searchBar';
 
-const API_KEY = process.env.API_KEY;
-const API_ID = process.env.API_ID;
 
 
 function Search() {
-    const [query, setQuery] = useState('')
-    const [recipe, setRecipe] = useState("");
-    const [loading, setLoading] = useState(false);
-
-    const options = {
-        method: "GET",
-        url: `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${API_ID}&app_key=${API_KEY}`,
-      };
-
-      const getRecipe = (e) => {
-        e.preventDefault();
-        setLoading(true);
-        axios
-          .request(options)
-          .then(function (response) {
-            setRecipe(response.data);
-            console.log(recipe)
-            setQuery("");
-            setLoading(false);
-          })
-          .catch(function (error) {
-            console.error(error);
-          });
-      };
+ 
   return (
     <>
     <Head>
@@ -45,32 +18,14 @@ function Search() {
       </Head>
     <Menubar />
     <Navbar />
-    <div className="p-4 justify-center">
-        <div className="relative flex items-center justify-center max-w-[500px] w-full m-auto pt-4 text-white z-10 ">
-          <form
-            onSubmit={getRecipe}
-            className="flex justify-between items-center w-full m-auto p-3 bg-transparent border border-gray-400 text-gray-400  rounded-2xl"
-          >
-            <div>
-              <input
-                onChange={(e) => setQuery(e.target.value)}
-                className="bg-transparent border-none text-gray-400 focus:outline-none text-2xl placeholder:text-white/15"
-                type="text"
-                placeholder="Search for a recipe"
-                value={query}
-              />
-            </div>
-            <button onClick={getRecipe}>
-              <HiSearch size={25} className='text-gray-400 fill-current' />
-            </button>
-          </form>
-        </div>
-        </div>
+    <SearchBar />
+
     </>
   )
 }
 
 export default Search;
+
 
 
 
